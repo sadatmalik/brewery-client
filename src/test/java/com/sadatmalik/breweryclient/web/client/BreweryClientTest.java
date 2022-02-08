a482b5e8c363016909771ecbf26a5377f15134eb
sadat.malik <sadat.malik@me.com>
2022-02-08 10:37:52 +0000

    initial client and test

---
+++ src/test/java/com/sadatmalik/breweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,25 @@
+package com.sadatmalik.breweryclient.web.client;
+
+import com.sadatmalik.breweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+
+    }
+}
